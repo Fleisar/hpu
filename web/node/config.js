4aa9df2d68b50777d05a997788a55d61dd594b61
Fleisar <48710716+Fleisar@users.noreply.github.com>
2020-11-11 20:20:04 +0700

    Update config.js

---
+++ web/node/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    sql: {
+        ip: '127.0.0.1',
+        port: 3306,
+        user: 'root',
+        password: '',
+        database: 'general'
+    },
+    db: {
+        users: 'hpu.users',
+        messages: 'hpu.messages'
+    }
+}
